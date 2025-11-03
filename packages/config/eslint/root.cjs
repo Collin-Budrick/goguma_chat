bb90aacb507f498d0d0a38927b2293f13936dd63
Collin Budrick <76412116+Collin-Budrick@users.noreply.github.com>
2025-11-02 20:14:56 -0500

    chore: centralize shared tooling config

---
+++ packages/config/eslint/root.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  ignorePatterns: [
+    "**/node_modules/**",
+    "**/.next/**",
+    "**/android/**",
+    "**/ios/**",
+    "**/.expo/**",
+    "**/dist/**",
+    "**/build/**",
+  ],
+};
